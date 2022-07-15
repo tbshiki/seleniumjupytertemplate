851b1bc608364b7b855c2ad1d77c2aa6f885a5fc
tbshiki <info@tbshiki.com>
2022-07-15 12:15:56 +0900

    Update Selenium4対応と要素追加、コメント修正

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ import sys
 from pathlib import Path
 
 on_py = False
-try:  # get_ipython().__class__.__name__ で判断させてもOK
-    # .py
-    path_file_dir = str(Path(__file__).resolve().parent)
+try:  # .py
+    path_file_dir = str(Path(__file__).resolve().parent)  # get_ipython().__class__.__name__ で判断させてもOK
     print("Hello, .py")
     on_py = True
 
-except:
-    # .ipynb
+except:  # .ipynb
     from IPython import get_ipython
 
     # 外部オリジナルモジュール変更時自動リロード
@@ -21,10 +19,11 @@ except:
     # %autoreload 2
 
     path_file_dir = str(Path().resolve())
+    print("Hello, .ipynb")
 
 sys.path.append(path_file_dir)
 sys.path.append(str(Path(path_file_dir).resolve().parent))
-sys.path.append(os.path.join(str(Path(path_file_dir).resolve().parent), +"module"))  # 自作モジュール置き場のパスを追加
+sys.path.append(os.path.join(str(Path(path_file_dir).resolve().parent), "module"))  # 自作モジュール置き場のパスを追加
 
 import pprint
 
@@ -42,14 +41,35 @@ import time
 from datetime import datetime, timedelta, timezone
 
 JST = timezone(timedelta(hours=+9), "JST")
+today = datetime.now(JST).strftime("%y%m%d")
 
 # %%
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 
+# from selenium.webdriver.support.select import Select
+# from selenium.webdriver.common.alert import Alert
+
+# ChromeDriverをダウンロードしなくていいようにするライブラリ pip install webdriver-manager
 from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
+
+# download_dir = "xxxxxxxxxxxx"
+# savefile_dir = "xxxxxxxxxxxx"
+# prefs = {
+#     "download.default_directory": download_dir,  # ダウンロード先のディレクトリ
+#     "savefile.default_directory": savefile_dir,  # 印刷保存先のディレクトリ
+# }
+# options.add_experimental_option("prefs", "")  # ファイルと印刷保存先を変更
+
+# options.add_experimental_option("detach", True)  # プログラムが終わってもブラウザを起動したままにする
+# options.add_argument("--kiosk-printing")  # 印刷ダイアログを表示せずそのまま印刷する
+
+# profile_dir = "xxxxxxxxxxxx"  # プロファイルのディレクトリ
+# options.add_argument("--user-data-dir=" + profile_dir) # プロファイルを変更
 
 if on_heroku == True:
     # Headless Chromeをあらゆる環境で起動させるオプション
@@ -60,13 +80,12 @@ if on_heroku == True:
     options.add_argument("--start-maximized")
     options.add_argument("--headless")
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 time.sleep(1)
 
 # %%
-# gspread スプレッドシートを
+# gspread Google スプレッドシート / pip install gspread
 # https://docs.gspread.org/
-# pip install gspread
 # oauth2client は非推奨となったので使わない
 
 import gspread
@@ -79,9 +98,9 @@ workbook = gc.open_by_key(SPREADSHEET_KEY)
 worksheet = workbook.sheet1  # 一番左のシートを指定
 
 # %%
-# xlwings Excel
+# xlwings Excel / pip install xlwings
 # https://docs.xlwings.org/
-# pip install xlwings
+# 必要に応じて xlwings addin install でアドオンをインストールすること
 
 import xlwings as xw
 
